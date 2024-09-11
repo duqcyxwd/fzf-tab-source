bf41adeb325ba637b0d4bd93823814681e3021ce
Yongqinchuan Du <duqcyxwd@gmail.com>
2024-09-11 10:12:48 -0400

    Customize the preview

--- sources/command.zsh
+++ sources/command.zsh
@@ -1,15 +1,34 @@
 # :fzf-tab:complete:(-command-:|command:option-(v|V)-rest)
+# source $ZDOTDIR/.zshenv
 case $group in
 'external command')
-  less =$word
+  echo "external command"
+  which "$word"
+  echo "---------"
+  bat =$word
   ;;
 'executable file')
-  less ${realpath#--*=}
+  echo "[Executable File]"
+  quick-preview ${realpath#--*=}
   ;;
 'builtin command')
+  echo "[Builtin Command]"
   run-help $word | bat -lman
   ;;
+'shell function')
+  echo '[Shell Function]'
+  which "$word"
+  ;;
+'alias')
+  echo "[Alias]"
+  which "$word"
+  ;;
 parameter)
+  echo "[Parameter]"
   echo ${(P)word}
+  ;;
+*)
+  echo "[Default]"
+  which "$word"
   ;;
 esac
